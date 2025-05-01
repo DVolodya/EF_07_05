ee39e7b1bd1ec7c6114e1930479f90998960fb06
Eugene Volodin <Euvolodin2@yandex.ru>
2025-05-01 21:54:51 +0300

    init

---
+++ src/main/java/sla/ef_07_05/Ef0705Application.java
@@ -0,0 +1,13 @@
+package sla.ef_07_05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ef0705Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ef0705Application.class, args);
+    }
+
+}
---
+++ src/test/java/sla/ef_07_05/Ef0705ApplicationTests.java
@@ -0,0 +1,13 @@
+package sla.ef_07_05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ef0705ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
